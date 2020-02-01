4e8e6dea12c9342ade064bb1fba41ca51a401e19
Shadow Tim <timothy.bellaiche@gmail.com>
2020-02-01 14:51:05 +0100

    started game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,11 @@
+﻿using FMOD.Studio;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private EventInstance _musicManager;
+    void Start()
+    {
+        // _musicManager = Utils.Sound.CreateSoundInstance("music");
+    }
+}
