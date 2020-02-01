3a78d163c954b895ca61ce25e059b96c54cb35a2
Damien <damiblanc@gmail.com>
2020-02-01 09:44:46 +0100

    Tilemap and tile switching around player

--- Assets/Scripts/Player/PlayerEntity.cs
+++ Assets/Scripts/Player/PlayerEntity.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 namespace Player
@@ -6,19 +5,24 @@ namespace Player
     public class PlayerEntity : MonoBehaviour
     {
         private PlayerMovementHandler _movementHandler;
+        private TileSwitcherHandler _tileSwitcherHandler;
 
         private void Awake()
         {
             PlayerManager.I.Player = this;
             _movementHandler = GetComponent<PlayerMovementHandler>();
             _movementHandler.Player = this;
+            _tileSwitcherHandler = GetComponent<TileSwitcherHandler>();
         }
 
         private void Update()
         {
             var direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-            
-            _movementHandler.MoveInDirection(direction.normalized, direction.magnitude);
+
+            if (direction != Vector2.zero)
+            {
+                _movementHandler.MoveInDirection(direction.normalized, direction.magnitude);
+            }
         }
     }
 }
