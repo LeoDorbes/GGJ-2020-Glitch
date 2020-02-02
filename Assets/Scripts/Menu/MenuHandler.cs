19805efc1ded16aea4af5d151cb78ec5caa56f72
Léo <leodorbes@gmail.com>
2020-02-02 12:17:28 +0100

    Added start menu

--- Assets/Scripts/Menu/MenuHandler.cs
+++ Assets/Scripts/Menu/MenuHandler.cs
@@ -1,12 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using DG.Tweening;
+
 
 namespace Menu
 {
     public class MenuHandler : MonoBehaviour
     {
+        [SerializeField] public Image blackImage;
+        
+        [SerializeField] public Image buttonImage;
+        
+        [SerializeField] public List<Image> images;
+
+        public static float frameDuration = 0.25f;
+        
         public void StartGame()
         {
+            StartCoroutine(launchGameCoroutine());
+        }
+
+        public IEnumerator launchGameCoroutine()
+        {
+            int i = 0;
+            foreach (var image in images)
+            {
+                i++;
+                if (i == images.Count)
+                {
+                    frameDuration = 3;
+
+                    blackImage.DOColor(new Color(0, 0, 0, 0), 3f);
+                }
+                buttonImage.sprite = image.sprite;
+                yield return new WaitForSeconds(frameDuration);
+            }
+            
             SceneManager.LoadScene("IntroDialogue");
         }
 
